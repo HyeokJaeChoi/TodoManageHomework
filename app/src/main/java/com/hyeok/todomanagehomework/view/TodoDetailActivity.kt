50e52d6378bd3c093fedba887d236d0b8e90cbaf
최혁재 <gurwo1007@gmail.com>
2020-06-25 00:49:15 +0900

    TodoDetailActivity 추가

---
+++ app/src/main/java/com/hyeok/todomanagehomework/view/TodoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.hyeok.todomanagehomework.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyeok.todomanagehomework.R
+
+class TodoDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_detail)
+    }
+}
