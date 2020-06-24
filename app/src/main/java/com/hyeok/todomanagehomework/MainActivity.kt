aae448e461e925c4b97a25327c3f7524080ac9f6
최혁재 <gurwo1007@gmail.com>
2020-06-24 14:17:29 +0900

    프로젝트 초기화 및 액션바 우측 메뉴 리소스 정의 & MainActivity에 inflate

---
+++ app/src/main/java/com/hyeok/todomanagehomework/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.hyeok.todomanagehomework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import java.time.LocalDateTime
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.let {
+            menuInflater.inflate(R.menu.todo_sort_option, menu)
+
+            return true
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.option_month -> {
+
+            }
+            R.id.option_week -> {
+
+            }
+            R.id.option_day -> {
+                
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
