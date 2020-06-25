61deee06944556a9e199b6a362c8bba274b68a2e
최혁재 <gurwo1007@gmail.com>
2020-06-25 10:51:07 +0900

    TodoDetailActivity 상단 앱바에 저장, 삭제 버튼에 사용할 이미지 리소스 추가 및 적용

--- app/src/main/java/com/hyeok/todomanagehomework/view/TodoDetailActivity.kt
+++ app/src/main/java/com/hyeok/todomanagehomework/view/TodoDetailActivity.kt
@@ -2,11 +2,34 @@ package com.hyeok.todomanagehomework.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.hyeok.todomanagehomework.R
 
 class TodoDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_detail)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.let {
+            menuInflater.inflate(R.menu.todo_detail_action, it)
+
+            return true
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.todo_detail_save_btn -> {
+
+            }
+            R.id.todo_detail_remove_btn -> {
+                
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
